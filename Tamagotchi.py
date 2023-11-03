ade023369866840774103b9f718292868759aa4a
Victorbarros081 <ead295vitinho@gmail.com>
2023-11-03 11:07:27 -0300

    Exercicio Tamagotchi

---
+++ Tamagotchi.py
@@ -0,0 +1,9 @@
+from biblioteca import Pessoa
+pessoa=Pessoa('Victor', '92', 25)
+pessoa.apresenta()
+pessoa.comer()
+pessoa.pararDeComer()
+pessoa.falar()
+pessoa.pararDeFalar()
+pessoa.dormir()
+pessoa.acordar()
